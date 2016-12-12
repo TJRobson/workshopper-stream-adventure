bee652ef72b054c82c518f2fc014c11d50da5b21
TJRobson <thomas.j.robson@hotmail.com>
2016-12-13 11:47:27 +1300

    lines exercise completed with both methods

---
+++ lines.js
@@ -0,0 +1,40 @@
+const through = require('through2')
+const split = require('split')
+
+const stream = through(write, end)
+
+let lineNum = 0
+
+function write (buffer, encoding, next) {
+  let line = buffer.toString()
+  this.push(lineNum % 2 === 0
+    ? line.toLowerCase() + '\n'
+    : line.toUpperCase() + '\n'
+  )
+  lineNum ++
+  next()
+}
+
+function end (done) {
+  done()
+}
+
+process.stdin
+  .pipe(split())
+  .pipe(stream)
+  .pipe(process.stdout)
+
+// let lineNum = 0
+// let tr = through(function (buffer, encoding, next) {
+//   let line = buffer.toString()
+//   this.push(lineNum % 2 === 0
+//     ? line.toLowerCase() + '\n'
+//     : line.toUpperCase() + '\n'
+//   )
+//   lineNum ++
+//   next()
+// })
+// process.stdin
+//   .pipe(split())
+//   .pipe(tr)
+//   .pipe(process.stdout)
