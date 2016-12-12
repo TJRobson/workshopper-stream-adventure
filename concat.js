793bb27d6170c53cdbae6843314b85c150af7547
TJRobson <thomas.j.robson@hotmail.com>
2016-12-13 12:40:16 +1300

    concat exercise complete, added their http/server/post example

---
+++ concat.js
@@ -0,0 +1,26 @@
+const concat = require('concat-stream')
+
+process.stdin.pipe(concat(function (thing) {
+  let trans = thing.toString().split('').reverse().join('')
+  console.log(trans)
+}))
+
+// Stream-Adventure's example:
+//
+// `concat-stream` is a write stream that you can pass a callback to get the
+// complete contents of a stream as a single buffer. Here's an example that uses
+// concat to buffer POST content in order to JSON.parse() the submitted data:
+//
+//     var concat = require('concat-stream')
+//     var http = require('http')
+//
+//     var server = http.createServer(function (req, res) {
+//         if (req.method === 'POST') {
+//             req.pipe(concat(function (body) {
+//                 var obj = JSON.parse(body)
+//                 res.end(Object.keys(obj).join('\n'))
+//             }))
+//         }
+//         else res.end()
+//     })
+//     server.listen(5000)
