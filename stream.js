cbd07a591da9b82b7c5505b7333c44a73b38984d
TJRobson <thomas.j.robson@hotmail.com>
2016-12-14 20:52:18 +1300

    stream task solved, added a better found solution

---
+++ stream.js
@@ -0,0 +1,28 @@
+const trumpet = require('trumpet')
+const through = require('through2')
+const tr = trumpet()
+
+const stream = through(write, end)
+
+const loud = tr.selectAll('.loud').createStream()
+
+loud.pipe(stream).pipe(loud)
+
+function write (buf, _, next) {
+  this.push(buf.toString().toUpperCase())
+  next()
+}
+
+function end (done) { done() }
+
+process.stdin.pipe(tr).pipe(process.stdout)
+
+
+// const selectWriteStream = tr.selectAll('.loud', (loud) => {
+//     const elementStream = loud.createStream()
+//     elementStream.on('data', (buf) => {
+//         elementStream.end(buf.toString().toUpperCase())
+//     })
+// })
+//
+// process.stdin.pipe(tr).pipe(process.stdout)
